e95bd0a772cb66706d308785a158025eb250275e
Emilio Ferrer <eferrer@correo.unimet.edu.ve>
2024-01-30 13:23:57 -0400

    FIRST COMMIT

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const button = document.getElementById('btn');
+button.addEventListener('mouseover', function () {
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+});
+button.addEventListener('click', function () {
+    alert('you clicked me')
+})
